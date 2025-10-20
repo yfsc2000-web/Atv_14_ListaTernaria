384d6ca7b94f8a3543f59e9b8c5dfb5d22958aa8
yfsc2000-web <yfsc2000@gmail.com>
2025-10-20 09:30:50 -0300

    Add files via upload

---
+++ Exercicio_15 (1).java
@@ -0,0 +1,15 @@
+package ExercíciosdeCondicionalTernáriaemJava;
+
+import java.util.Scanner;
+
+public class Exercicio_15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a idade?");
+        int idade = sc.nextInt();
+        String resultado;
+        resultado= (idade > 16)? "Pode votar" : "Não pode votar";
+        System.out.println(resultado);
+
+    }
+}
---
+++ Exercicio_16 (1).java
@@ -0,0 +1,16 @@
+package ExercíciosdeCondicionalTernáriaemJava;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Exercicio_16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um número inteiro:");
+        int numero = sc.nextInt();
+        String resultado;
+        resultado= (numero% 5==0)?"É multiplo" : "Não é multiplo";
+        System.out.println(resultado);
+
+    }
+}
